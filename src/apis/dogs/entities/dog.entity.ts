696fe3ec087ada9aca8e0b448275acb07d7c1091
liberty556 <liberty556@gmail.com>
2023-03-15 15:48:30 +0900

    feat: dogs
    Create dog entity

---
+++ src/apis/dogs/entities/dog.entity.ts
@@ -0,0 +1,46 @@
+import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Dog {
+	@PrimaryGeneratedColumn('uuid')
+	@Field(() => String)
+	id: string;
+
+	@Column({
+		length: 10,
+	})
+	@Field(() => String)
+	name: string;
+
+	@Column()
+	@Field(() => Int)
+	age: number;
+
+	@Column({
+		type: 'decimal',
+		precision: 4,
+		scale: 1,
+	})
+	@Field(() => Float)
+	weight: number;
+
+	@Column({
+		length: 20, //
+	})
+	@Field(() => String)
+	breed: string;
+
+	@Column({
+		type: 'text', //
+	})
+	@Field(() => String)
+	specifics: string;
+
+	@Column({
+		length: 100, //
+	})
+	@Field(() => String)
+	image: string;
+}
