1b1b2d90fdc009a267c21624afc2a2c92224151f
liberty556 <liberty556@gmail.com>
2023-03-15 17:07:47 +0900

    feat: dogs
    App.module.ts에서 TypeOrmModule 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DogsModule } from './apis/dogs/dogs.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +7,16 @@ import { AppService } from './app.service';
 @Module({
 	imports: [
 		DogsModule, //
+		TypeOrmModule.forRoot({
+			type: process.env.DATABASE_TYPE as 'mysql',
+			host: process.env.DATABASE_HOST,
+			port: Number(process.env.DATABASE_PORT),
+			username: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASEA_PASSWORD,
+			entities: [__dirname + '/apis/**/*.entity.*'],
+			logging: true,
+			synchronize: true,
+		}),
 	],
 	controllers: [AppController],
 	providers: [AppService],
