5e45f357fc77b9d4747874d0fc5495125c434d32
YR8002 <120006167+YR8002@users.noreply.github.com>
2023-03-16 17:31:35 +0900

    chore: GraphQL describtion 추가 - shops.resolver.ts 파일에서 쿼리문의 리턴에 대한 설명을 추가했습니다

--- src/apis/shops/shops.resolver.ts
+++ src/apis/shops/shops.resolver.ts
@@ -10,31 +10,41 @@ export class ShopsResolver {
 		private readonly shopsService: ShopsService, //
 	) {}
 
-	@Query(() => [Shop])
+	@Query(() => [Shop], {
+		description: 'Return : DB에 등록된 모든 가게(Shop) 데이터',
+	})
 	async fetchShops(): Promise<Shop[]> {
 		return await this.shopsService.findAll();
 	}
 
-	@Query(() => Shop)
+	@Query(() => Shop, {
+		description: 'Return : 입력한 shopId와 일치하는 가게(Shop) 데이터',
+	})
 	async fetchShop(
 		@Args('shopId') shopId: string, //
 	): Promise<Shop> {
 		return await this.shopsService.findById({ shopId });
 	}
 
-	@Query(() => [Shop])
+	@Query(() => [Shop], {
+		description: 'Return : DB에 등록된 모든 삭제된 가게(Shop) 데이터',
+	})
 	async fetchShopsWithDeleted(): Promise<Shop[]> {
 		return await this.shopsService.findAllDeleted();
 	}
 
-	@Query(() => Shop)
+	@Query(() => Shop, {
+		description: 'Return : 입력한 shopId와 일치하는 가게(Shop) 데이터',
+	})
 	async fetchShopWithDeleted(
 		@Args('shopId') shopId: string, //
 	): Promise<Shop> {
 		return await this.shopsService.findDeleted({ shopId });
 	}
 
-	@Mutation(() => Shop)
+	@Mutation(() => Shop, {
+		description: 'Return : 새로 생성되어 DB에 저장된 신규 가게(Shop) 데이터',
+	})
 	createShop(
 		@Args('createShopInput') createShopInput: CreateShopInput,
 	): Promise<Shop> {
@@ -42,7 +52,10 @@ export class ShopsResolver {
 		return this.shopsService.create({ address, createShopInput });
 	}
 
-	@Mutation(() => Shop)
+	@Mutation(() => Shop, {
+		description:
+			'Return : 입력된 데이터로 수정되어 DB에 저장된 가게(Shop) 데이터',
+	})
 	async updateShop(
 		@Args('shopId') shopId: string,
 		@Args('updateShopInput') updateShopInput: UpdateShopInput,
@@ -50,14 +63,19 @@ export class ShopsResolver {
 		return await this.shopsService.update({ shopId, updateShopInput });
 	}
 
-	@Mutation(() => Boolean)
+	@Mutation(() => Boolean, {
+		description:
+			'Return : 가게 정보 삭제 완료 시 true. softdelete이므로 가게의 정보를 복원할 수 있습니다',
+	})
 	async deleteShop(
 		@Args('shopId') shopId: string, //
 	): Promise<boolean> {
 		return await this.shopsService.delete({ shopId });
 	}
 
-	@Mutation(() => Boolean)
+	@Mutation(() => Boolean, {
+		description: 'Return : 가게 정보 복구 완료 시 true.',
+	})
 	async restoreShop(
 		@Args('shopId') shopId: string, //
 	): Promise<boolean> {
