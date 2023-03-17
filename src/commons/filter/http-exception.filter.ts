bee8b031f188f2ec6120636851627bdaa1d2e9cd
YR8002 <120006167+YR8002@users.noreply.github.com>
2023-03-17 19:32:05 +0900

    chore: 예약 관련 API에서 조인 해제 후 graphQL 테스트 완료(2)

---
+++ src/commons/filter/http-exception.filter.ts
@@ -0,0 +1,31 @@
+import {
+	ArgumentsHost,
+	Catch,
+	ExceptionFilter,
+	HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter<HttpException> {
+	catch(exception: HttpException, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const response = ctx.getResponse<Response>();
+		const request = ctx.getRequest<Request>();
+		const statusCode = exception.getStatus();
+		const message = exception.message;
+
+		console.log('============');
+		console.log('예외가 발생했어요!');
+		console.log('예외내용:', message);
+		console.log('예외코드:', statusCode);
+		console.log(response);
+		console.log('============');
+
+		// response.status(statusCode).json({
+		// 	statusCode,
+		// 	timestamp: new Date().toISOString(),
+		// 	path: request.url,
+		// });
+	}
+}
