9cb60127658d58b4d8e9c4b2f053587f84308e2a
YR8002 <120006167+YR8002@users.noreply.github.com>
2023-03-17 17:55:21 +0900

    chore: 앱모듈 파일 수정

---
+++ src/app.module.ts
@@ -0,0 +1,51 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthModule } from './apis/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DogsModule } from './apis/dogs/dogs.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CacheModule, Module } from '@nestjs/common';
+import { ShopsModule } from './apis/shops/shops.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './apis/users/user.module';
+import { JwtAccessStrategy } from './apis/auth/strategies/jwt-access.strategy';
+import { JwtRefreshStrategy } from './apis/auth/strategies/jwt-refresh.stratehy';
+import { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
+import { JwtGoogleStrategy } from './apis/auth/strategies/jwt-social-google.strategy';
+
+@Module({
+	imports: [
+		AuthModule, //
+		DogsModule,
+		ShopsModule,
+		UsersModule,
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: true,
+			context: ({ req, res }) => ({ req, res }),
+		}),
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: process.env.DATABASE_TYPE as 'mysql',
+			host: process.env.DATABASE_HOST,
+			port: Number(process.env.DATABASE_PORT),
+			username: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASE_PASSWORD,
+			database: process.env.DATABASE_DATABASE,
+			entities: [__dirname + '/apis/**/*.entity.*'],
+			synchronize: true,
+			logging: true,
+		}),
+		CacheModule.register<RedisClientOptions>({
+			store: redisStore,
+			url: 'redis://my-redis:6379',
+			isGlobal: true,
+		}),
+	],
+	providers: [
+		JwtAccessStrategy, //
+		JwtRefreshStrategy,
+		JwtGoogleStrategy,
+	],
+})
+export class AppModule {}
