8397380b6c5b0e09b6071d5438ae0b629536d058
Jjoobob123 <273hur4747@gmail.com>
2023-03-23 22:25:15 +0900

    feat: User 테스트코드 추가 및 prod 파일 추가
    User 테스트코드 추가 및 prod 파일 추가

--- src/apis/users/dto/update-users.input.ts
+++ src/apis/users/dto/update-users.input.ts
@@ -1,5 +1,16 @@
-import { InputType, PartialType } from '@nestjs/graphql';
-import { CreateUserInput } from './create-users.input';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {}
+export class UpdateUserInput {
+	@Field(() => String, { nullable: true })
+	name: string;
+
+	@Field(() => String, { nullable: true })
+	password: string;
+
+	@Field(() => String, { nullable: true })
+	phone: string;
+
+	@Field(() => String, { nullable: true })
+	image: string;
+}
