9dd9663ec2f9819510e30af80002961450082f7a
YR8002 <120006167+YR8002@users.noreply.github.com>
2023-03-27 21:20:14 +0900

    test: 가게이미지 리졸버 테스트코드 완료

--- src/apis/shopImages/__test__/shopImage.moking.dummy.ts
+++ src/apis/shopImages/__test__/shopImage.moking.dummy.ts
@@ -1,65 +1,89 @@
-export class MockShopImagesRepository {
+export class MockShopImagesResolver {
 	mydb = [
 		{
-			//
 			id: '370b960e-55d5-445b-935b-9fdfee36955c',
 			imageUrl: 'Test-url-111-asdfsadfasdf-asdfsadf',
 			isThumbnail: false,
 			shop: {
-				//
 				id: '69f836c4-e0e4-4841-960d-2be40d150c44',
 			},
 		},
 		{
-			//
 			id: '1e31187d-5c71-4bff-b124-d01081306e07',
 			imageUrl: 'Test-url-222-asdfsadfasdf-asdfsadf',
 			isThumbnail: true,
 			shop: {
-				//
 				id: '69f836c4-e0e4-4841-960d-2be40d150c44',
 			},
 		},
 		{
-			//
 			id: 'c65366c0-2b7b-4a79-bff5-e4d8659a5e76',
 			imageUrl: 'Test-url-333-asdfsadfasdf-asdfsadf',
 			isThumbnail: false,
 			shop: {
-				//
 				id: 'fecbeaa7-f83c-4cb6-a82f-ebbc1197e20e',
 			},
 		},
 		{
-			//
 			id: '958472c6-0dea-40c0-b9db-dbfa11cd630e',
 			imageUrl: 'Test-url-444-asdfsadfasdf-asdfsadf',
 			isThumbnail: false,
 			shop: {
-				//
 				id: '69f836c4-e0e4-4841-960d-2be40d150c44',
 			},
 		},
 	];
 
-	// id로 찾아오는 findOne
-	findOne({ where }) {
-		const shopImage = this.mydb.filter((el) => el.id === where.id);
+	findThumbnailByShopId({ shopId }) {
+		const shopImage = this.mydb.filter(
+			(el) => el.shop.id === shopId && el.isThumbnail === true,
+		);
 		if (shopImage.length) {
 			return shopImage[0];
 		}
 		return null;
 	}
 
-	// 저장
-	save({ imageUrl, isThumbnail, shopId }) {
-		const id = this.uuidv4();
-		this.mydb.push({ id, imageUrl, isThumbnail, shop: { id: shopId } });
-		return { imageUrl, isThumbnail, shop: { id: shopId } };
+	findByShopId({ shopId }) {
+		const shopImage = this.mydb.filter((el) => el.shop.id === shopId);
+		if (shopImage.length) {
+			return shopImage;
+		}
+		return null;
 	}
 
-	// id 기준으로 삭제
-	delete({ where }) {
+	save({ imageUrl, isThumbnail, shopId }) {
+		const _id = this.uuidv4();
+		this.mydb.push({
+			id: _id,
+			imageUrl,
+			isThumbnail,
+			shop: { id: shopId },
+		});
+		const _save = this.mydb.filter((el) => el.id === _id)[0];
+		return _save;
+	}
+
+	updateShopImage({ updateShopImageInput }) {
+		const { id, imageUrl, isThumbnail, shopId } = updateShopImageInput;
+		this.deleteShopImage({ where: { id } });
+		this.mydb.push({
+			id,
+			imageUrl,
+			isThumbnail,
+			shop: { id: shopId },
+		});
+		const result = {
+			id,
+			imageUrl,
+			isThumbnail,
+			shop: { id: shopId },
+		};
+
+		return result;
+	}
+
+	deleteShopImage({ where }) {
 		const shopImage = this.mydb.filter((el) => el.id === where.id);
 		const shopImageIdx = this.mydb.findIndex((el) => el.id === where.id);
 		if (shopImage.length) {
--- src/apis/shopImages/__test__/shopImage.resolver.spec.ts
+++ src/apis/shopImages/__test__/shopImage.resolver.spec.ts
@@ -1,23 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { ShopImage } from '../entities/shopImages.entity';
-import { ShopImagesResolver } from '../shopImage.resolver';
-import { ShopImagesService } from '../shopImage.service';
-import { MockShopImagesRepository } from './shopImage.moking.dummy';
+import { MockShopImagesResolver } from './shopImage.moking.dummy';
 
-const shopImageResolverFetchThumbnailByShop = //
-	jest.fn(() => ShopImage);
-const shopImageResolverFetchShopImagesByShop = jest.fn(() =>
-	//
-	jest.fn(() => ShopImage),
-);
-const shopImageResolverCreateShopImage = jest.fn(() =>
-	//
-	jest.fn(() => ShopImage),
-);
-const shopImageResolverDeleteShopImage = jest.fn(() => true);
-const Example_ShopIamge = {
+const Example_ShopImage = {
 	id: '1e31187d-5c71-4bff-b124-d01081306e07',
 	imageUrl: 'Test-url-222-asdfsadfasdf-asdfsadf',
 	isThumbnail: true,
@@ -26,110 +10,95 @@ const Example_ShopIamge = {
 	},
 };
 
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
-
 describe('shopImagesResolver', () => {
-	let mockShopImagesRepository: MockShopImagesRepository;
+	let mockShopImagesResolver: MockShopImagesResolver;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			imports: [
-				//
-			],
 			providers: [
-				//
-				ShopImagesResolver,
 				{
-					provide: ShopImagesResolver,
-					useClass: jest.fn(() => ({
-						//
-						fetchThumbnailByShop: shopImageResolverFetchThumbnailByShop,
-						fetchShopImagesByShop: shopImageResolverFetchShopImagesByShop,
-						createShopImage: shopImageResolverCreateShopImage,
-						deleteShopImage: shopImageResolverDeleteShopImage,
-					})),
+					provide: MockShopImagesResolver,
+					useClass: MockShopImagesResolver,
 				},
 			],
 		}).compile();
-
-		mockShopImagesRepository = module.get(getRepositoryToken(ShopImage));
+		mockShopImagesResolver = module.get(MockShopImagesResolver);
 	});
 
-	describe('shopImagesResolver', () => {
+	describe('fetchThumbnailByShop', () => {
 		it('가게이미지 중 썸네일 리턴해야함', async () => {
-			const shopId = Example_ShopIamge.id;
-			const myThumbnail = await mockShopImagesRepository.findOne({
-				where: { isThumbnail: true, shop: { id: shopId } },
+			const shopId = Example_ShopImage.shop.id;
+			const myThumbnail = await mockShopImagesResolver.findThumbnailByShopId({
+				shopId,
 			});
-			const result = myThumbnail.isThumbnail;
-
-			expect(result).toBe(true);
+			expect(myThumbnail.isThumbnail).toEqual(true);
 		});
+	});
 
-		it('가게이미지 리턴해야함', async () => {
-			const shopId = Example_ShopIamge.id;
-			const result = await mockShopImagesRepository.findOne({
-				where: { shop: { id: shopId } },
+	describe('fetchShopImagesByShop', () => {
+		it('가게이미지 배열 리턴해야함', async () => {
+			const shopId = Example_ShopImage.shop.id;
+			const result = await mockShopImagesResolver.findByShopId({
+				shopId,
 			});
 			const rightReturn = [
 				{
-					//
 					id: '370b960e-55d5-445b-935b-9fdfee36955c',
 					imageUrl: 'Test-url-111-asdfsadfasdf-asdfsadf',
 					isThumbnail: false,
 					shop: {
-						//
 						id: '69f836c4-e0e4-4841-960d-2be40d150c44',
 					},
 				},
 				{
-					//
 					id: '1e31187d-5c71-4bff-b124-d01081306e07',
 					imageUrl: 'Test-url-222-asdfsadfasdf-asdfsadf',
 					isThumbnail: true,
 					shop: {
-						//
 						id: '69f836c4-e0e4-4841-960d-2be40d150c44',
 					},
 				},
 				{
-					//
 					id: '958472c6-0dea-40c0-b9db-dbfa11cd630e',
 					imageUrl: 'Test-url-444-asdfsadfasdf-asdfsadf',
 					isThumbnail: false,
 					shop: {
-						//
 						id: '69f836c4-e0e4-4841-960d-2be40d150c44',
 					},
 				},
 			];
-
 			expect(result).toEqual(rightReturn);
 		});
+	});
 
+	describe('createShopImage', () => {
 		it('가게이미지 리턴해야함', async () => {
-			const input = Example_ShopIamge;
-			const result = await mockShopImagesRepository.findOne({
-				where: { id: input.id },
+			const input = Example_ShopImage;
+			const result = await mockShopImagesResolver.save({
+				imageUrl: Example_ShopImage.imageUrl,
+				isThumbnail: Example_ShopImage.isThumbnail,
+				shopId: Example_ShopImage.shop.id,
 			});
 
-			expect(result).toEqual(input);
+			expect(result).toHaveProperty('id');
+			expect(result).toHaveProperty('imageUrl');
+			expect(result).toHaveProperty('isThumbnail');
+			expect(result).toHaveProperty('shop');
 		});
+	});
 
+	describe('deleteShopImage', () => {
 		it('boolean 리턴해야함', async () => {
-			const input = Example_ShopIamge;
-			const mydata = await mockShopImagesRepository.findOne({
-				where: { id: input.id },
-			});
-			const removeIdx = mockShopImagesRepository.mydb.findIndex(
+			const input = Example_ShopImage;
+			const removeIdx = mockShopImagesResolver.mydb.findIndex(
 				(el) => el.id === input.id,
 			);
 
 			if (removeIdx !== -1) {
-				mockShopImagesRepository.mydb.splice(removeIdx, 1);
+				mockShopImagesResolver.mydb.splice(removeIdx, 1);
 			}
 
-			const result = mockShopImagesRepository.mydb.some(
+			const result = mockShopImagesResolver.mydb.some(
 				(el) => el.id === input.id,
 			);
 
