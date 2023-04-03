0996afdb9eeadd22f81ed3830ba763c19bc2d014
YR8002 <120006167+YR8002@users.noreply.github.com>
2023-04-04 01:53:36 +0900

    refact: shopImages의 리졸버 테스트코드 리팩토링

--- src/apis/shopImages/__test__/shopImage.resolver.spec.ts
+++ src/apis/shopImages/__test__/shopImage.resolver.spec.ts
@@ -1,123 +1,170 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { MockShopImagesResolver } from './shopImage.moking.dummy';
+import { ShopImagesResolver } from '../shopImage.resolver';
+import { ShopImagesService } from '../shopImage.service';
+import { ShopImage } from '../entities/shopImages.entity';
+import { UpdateShopImageInput } from '../dto/update-shopImage.input';
+import { Shop } from 'src/apis/shops/entities/shop.entity';
 
-const Example_ShopImage = {
+// jest.mock('../shopImage.resolver');
+jest.mock('../shopImage.service');
+
+const EXAMPLE_SHOP: Shop = {
+	id: '1',
+	name: '1',
+	phone: '1',
+	openHour: '1',
+	closeHour: '1',
+	address: '1',
+	code: 123,
+	lat: '1',
+	lng: '1',
+	averageStar: 3,
+	reservation: null,
+	image: null,
+	review: null,
+	updatedAt: new Date(),
+	deletedAt: null,
+};
+
+const EXAMPLE_SHOP_IMAGE: ShopImage = {
 	id: '1e31187d-5c71-4bff-b124-d01081306e07',
 	imageUrl: 'Test-url-222-asdfsadfasdf-asdfsadf',
 	isThumbnail: true,
-	shop: {
-		id: '69f836c4-e0e4-4841-960d-2be40d150c44',
-	},
+	shop: EXAMPLE_SHOP,
 };
-const rightReturn = [
-	{
-		id: '370b960e-55d5-445b-935b-9fdfee36955c',
-		imageUrl: 'Test-url-111-asdfsadfasdf-asdfsadf',
-		isThumbnail: false,
-		shop: {
-			id: '69f836c4-e0e4-4841-960d-2be40d150c44',
-		},
-	},
-	{
-		id: '1e31187d-5c71-4bff-b124-d01081306e07',
-		imageUrl: 'Test-url-222-asdfsadfasdf-asdfsadf',
-		isThumbnail: true,
-		shop: {
-			id: '69f836c4-e0e4-4841-960d-2be40d150c44',
-		},
-	},
-	{
-		id: '958472c6-0dea-40c0-b9db-dbfa11cd630e',
-		imageUrl: 'Test-url-444-asdfsadfasdf-asdfsadf',
-		isThumbnail: false,
-		shop: {
-			id: '69f836c4-e0e4-4841-960d-2be40d150c44',
-		},
-	},
-];
 
-describe('shopImagesResolver', () => {
-	let mockShopImagesResolver: MockShopImagesResolver;
+describe('ShopImagesResolver', () => {
+	let shopImagesResolver: ShopImagesResolver;
+	let shopImagesService: ShopImagesService;
 
 	beforeEach(async () => {
-		const module: TestingModule = await Test.createTestingModule({
+		const moduleRef = await Test.createTestingModule({
 			providers: [
+				ShopImagesResolver,
 				{
-					provide: MockShopImagesResolver,
-					useClass: MockShopImagesResolver,
+					provide: ShopImagesService,
+					useValue: {
+						findThumbnailByShopId: jest.fn(),
+						findByShopId: jest.fn(),
+						save: jest.fn(),
+						update: jest.fn(),
+						delete: jest.fn(),
+					},
 				},
 			],
 		}).compile();
-		mockShopImagesResolver = module.get(MockShopImagesResolver);
+
+		shopImagesResolver = moduleRef.get<ShopImagesResolver>(ShopImagesResolver);
+		shopImagesService = moduleRef.get<ShopImagesService>(ShopImagesService);
 	});
 
 	describe('fetchThumbnailByShop', () => {
-		it('가게이미지 중 썸네일 리턴해야함', async () => {
-			const shopId = Example_ShopImage.shop.id;
-			const myThumbnail = await mockShopImagesResolver.findThumbnailByShopId({
-				shopId,
-			});
-			expect(myThumbnail.isThumbnail).toEqual(true);
+		it('should return a ShopImage object', async () => {
+			const shopImageMock: ShopImage = {
+				id: '1',
+				imageUrl: 'https://test.com/image.jpg',
+				isThumbnail: true,
+				shop: EXAMPLE_SHOP,
+			};
+			jest
+				.spyOn(shopImagesService, 'findThumbnailByShopId')
+				.mockResolvedValueOnce(shopImageMock);
+
+			const result = await shopImagesResolver.fetchThumbnailByShop('1');
+
+			expect(result).toBe(shopImageMock);
 		});
 	});
 
 	describe('fetchShopImagesByShop', () => {
-		it('가게이미지 배열 리턴해야함', async () => {
-			const shopId = Example_ShopImage.shop.id;
-			const result = await mockShopImagesResolver.findByShopId({
-				shopId,
-			});
+		it('should return an array of ShopImage objects', async () => {
+			const shopImagesMock: ShopImage[] = [
+				{
+					id: '1',
+					imageUrl: 'https://test.com/image1.jpg',
+					isThumbnail: true,
+					shop: EXAMPLE_SHOP,
+				},
+				{
+					id: '2',
+					imageUrl: 'https://test.com/image2.jpg',
+					isThumbnail: false,
+					shop: EXAMPLE_SHOP,
+				},
+			];
+			jest
+				.spyOn(shopImagesService, 'findByShopId')
+				.mockResolvedValueOnce(shopImagesMock);
 
-			expect(result).toMatchObject(rightReturn);
+			const result = await shopImagesResolver.fetchShopImagesByShop('1');
+
+			expect(result).toEqual(shopImagesMock);
 		});
 	});
 
 	describe('createShopImage', () => {
-		it('생성한 가게이미지 리턴해야함', async () => {
-			const input = Example_ShopImage;
-			const result = await mockShopImagesResolver.save({
-				imageUrl: Example_ShopImage.imageUrl,
-				isThumbnail: Example_ShopImage.isThumbnail,
-				shopId: Example_ShopImage.shop.id,
-			});
+		it('should return a ShopImage object', async () => {
+			const input = {
+				imageUrl: 'https://test.com/image.jpg',
+				isThumbnail: true,
+				shopId: EXAMPLE_SHOP.id,
+			};
+			const shopImageMock: ShopImage = {
+				id: '1',
+				imageUrl: input.imageUrl,
+				isThumbnail: input.isThumbnail,
+				shop: EXAMPLE_SHOP,
+			};
+			jest
+				.spyOn(shopImagesService, 'save')
+				.mockResolvedValueOnce(shopImageMock);
 
-			expect(result).toHaveProperty('id');
-			expect(result).toHaveProperty('imageUrl');
-			expect(result).toHaveProperty('isThumbnail');
-			expect(result).toHaveProperty('shop');
+			const result = await shopImagesResolver.createShopImage(
+				input.imageUrl,
+				input.isThumbnail,
+				input.shopId,
+			);
+
+			expect(result).toBe(shopImageMock);
 		});
 	});
 
 	describe('updateShopImage', () => {
-		it('업데이트한 가게이미지 리턴해야함', async () => {
-			const updateShopImageInput = Example_ShopImage;
-			const result = await mockShopImagesResolver.updateShopImage({
-				updateShopImageInput,
-			});
+		const mockShopImage: ShopImage = {
+			id: '123',
+			imageUrl: 'https://example.com/image.png',
+			isThumbnail: true,
+			shop: EXAMPLE_SHOP,
+		};
 
-			expect(result).toHaveProperty('id');
-			expect(result).toHaveProperty('imageUrl');
-			expect(result).toHaveProperty('isThumbnail');
-			expect(result).toHaveProperty('shop');
+		const mockUpdateShopImageInput: UpdateShopImageInput = {
+			id: '123',
+			imageUrl: 'https://example.com/image.png',
+			isThumbnail: true,
+			shopId: EXAMPLE_SHOP.id,
+		};
+
+		jest.spyOn(shopImagesService, 'update').mockResolvedValue(mockShopImage);
+
+		it('업데이트한 ShopImage 리턴해야함', async () => {
+			const result = await shopImagesResolver.updateShopImage(
+				mockUpdateShopImageInput,
+			);
+			expect(result).toEqual(mockShopImage);
 		});
 	});
 
 	describe('deleteShopImage', () => {
-		it('boolean 리턴해야함', async () => {
-			const input = Example_ShopImage;
-			const removeIdx = mockShopImagesResolver.mydb.findIndex(
-				(el) => el.id === input.id,
-			);
+		it('삭제 후 true 리턴해아함', async () => {
+			const shopImageId = 'abc123';
+			const expectedResult = true;
 
-			if (removeIdx !== -1) {
-				mockShopImagesResolver.mydb.splice(removeIdx, 1);
-			}
+			jest.spyOn(shopImagesService, 'delete').mockResolvedValue(expectedResult);
 
-			const result = mockShopImagesResolver.mydb.some(
-				(el) => el.id === input.id,
-			);
+			const result = await shopImagesResolver.deleteShopImage(shopImageId);
 
-			expect(result).toBe(false);
+			expect(shopImagesService.delete).toHaveBeenCalledWith({ shopImageId });
+			expect(result).toEqual(expectedResult);
 		});
 	});
 });
